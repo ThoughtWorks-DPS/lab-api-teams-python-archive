bab5ce38a62f91d181f636ef62aeade3e5c79a2c
Patrick McFadden <pmcfadde@thoughtworks.com>
2022-07-29 15:11:52 -0600

    [pmcfadden] combining local directory

---
+++ local/local_prestart.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+
+export $(cat local.env | xargs)
+
+localhost="--endpoint-url ${DYNAMODB_URL}"
+
+if aws dynamodb describe-table ${localhost} --table-name ${DYNAMODB_TABLE_NAME} > /dev/null 2>&1; then
+    echo "Table: ${DYNAMODB_TABLE_NAME} already exists..."
+else
+    echo "Creating table...: ${DYNAMODB_TABLE_NAME}"
+    aws dynamodb create-table --table-name ${DYNAMODB_TABLE_NAME} ${localhost} --cli-input-json file://dynamodb_table_schema.json
+    echo "${DYNAMODB_TABLE_NAME} has been created..."
+fi
+
+exec "$@"
